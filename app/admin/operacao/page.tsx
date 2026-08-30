01f24353952886da1dc10a4e0dc360b265777983
Gessicatd <topnailsbrazil@gmail.com>
2026-08-30 19:56:49 -0300

    fix: publish operations dashboard route

---
+++ app/admin/operacao/page.tsx
@@ -0,0 +1,7 @@
+import OperationsDashboard from "./OperationsDashboard";
+
+export const dynamic = "force-dynamic";
+
+export default function OperationsPage() {
+  return <OperationsDashboard />;
+}
